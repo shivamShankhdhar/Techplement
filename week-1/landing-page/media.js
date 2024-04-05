c85dfbbc474781b7b578c009d057ff1ee2a6fa56
shivamShankhdhar <s.shankhdhar1981@gmail.com>
2024-04-05 21:53:52 +0530

    footer ready

--- week-1/landing-page/media.js
+++ week-1/landing-page/media.js
@@ -1,23 +1,15 @@
 const openMenuBtn = document.getElementById("open-menu");
-
 const closeMenuButton = document.getElementById("close-menu");
-
-const listItem0 = document.getElementsByClassName("list-items-0")[0];
-
-const listItem1 = document.getElementsByClassName("list-items-1")[0];
+const navLinks = document.getElementsByClassName("nav-links")[0];
 
 openMenuBtn.addEventListener("click", () => {
   closeMenuButton.style.display = "block"
   openMenuBtn.style.display = "none"
-  // alert("clicked")
-  listItem0.style.display = "grid"
-  listItem1.style.display = "grid"
+  navLinks.style.display = "block"
 })
 
 closeMenuButton.addEventListener("click", () => {
   openMenuBtn.style.display = "block"
   closeMenuButton.style.display = "none"
-  // alert("clicked")
-  listItem0.style.display = "none"
-  listItem1.style.display = "none"
+  navLinks.style.display = "none"
 })
